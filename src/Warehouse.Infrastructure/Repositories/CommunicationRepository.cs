00ad28ddad82ac8789b4a909365090dc4f8b5abd
325972brian <81413816+325972brian@users.noreply.github.com>
2022-04-08 11:03:46 +0200

    Initial commit

---
+++ src/Warehouse.Infrastructure/Repositories/CommunicationRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using Warehouse.Domain.Entities;
+using Warehouse.Application.Interfaces;
+
+namespace Warehouse.Infrastructure.Repositories
+{
+    /// <summary>
+    /// Communication repositiry.
+    /// </summary>
+    public class CommunicationRepository : Repository<CommunicationEntity>, ICommunicationRepository
+    {
+        /// <summary>
+        /// Dependency of DbContext.
+        /// </summary>
+        public new WarehouseDbContext Context { get; }
+
+        public new ILogger<CommunicationRepository> Logger { get; }
+
+        /// <summary>
+        /// CommunicationRepository constructor. 
+        /// </summary>
+        /// <param name="context"></param>
+        public CommunicationRepository(WarehouseDbContext context, ILogger<CommunicationRepository> logger) : base(context, logger)
+        {
+            Context = context;
+            Logger = logger;
+        }
+    }
+}
